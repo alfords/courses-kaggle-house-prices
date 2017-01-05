1b529bbd46c242ac35c0bad156240ce4cd926d04
cam-martin <cameron@datacamp.com>
2017-01-05 12:23:44 -0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -2,3 +2,4 @@ pip3 install numpy==1.11.2 --upgrade
 pip3 install pandas==0.19.1 --upgrade
 pip3 install scikit-learn==0.18.1 --upgrade
 pip3 install xgboost==0.6a2 --upgrade
+pip3 install seaborn==0.7.1 --upgrade
